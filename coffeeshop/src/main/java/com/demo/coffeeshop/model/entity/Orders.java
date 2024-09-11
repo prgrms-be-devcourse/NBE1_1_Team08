65dfe9e8a6567de7482c8d4ba0dcf872ef5d7053
pyy2114 <pyy2114@naver.com>
2024-09-11 14:55:46 +0900

    [feat] 배송 상태에 따른 주문내역 취소, 수정 제약 기능 구현

--- coffeeshop/src/main/java/com/demo/coffeeshop/model/entity/Orders.java
+++ coffeeshop/src/main/java/com/demo/coffeeshop/model/entity/Orders.java
@@ -78,8 +78,12 @@ public class Orders {
     }
 
     public void changeInfo(String address, String postcode){
-        this.address = address;
-        this.postcode = postcode;
+        if(this.orderStatus == OrderStatus.ACCEPTED || this.orderStatus == OrderStatus.PAYMENT_CONFIRMED){
+            this.address = address;
+            this.postcode = postcode;
+        }else{
+            throw new IllegalStateException("배송이 시작되어 주문 변경이 불가합니다.");
+        }
     }
 
     public void changeStatus(OrderStatus orderStatus){
